4716cb63a174878ca9e0d270dff76dcc4a8c5367
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-05-11 07:43:53 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,8 @@
+<form action="insert.php" method="POST">
+    <input type="text" name="event_day" placeholder="اليوم" required>
+    <input type="text" name="event_date" placeholder="التاريخ (هجري)" required>
+    <input type="text" name="host_name" placeholder="صاحب المناسبة" required>
+    <input type="text" name="location" placeholder="المكان (الديرة/جدة)" required>
+    <input type="text" name="hall_name" placeholder="اسم القاعة/القصر" required>
+    <button type="submit">إضافة المناسبة</button>
+</form>
